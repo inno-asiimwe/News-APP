57f3c0e5140c8cdb0ce9abe4fc3a00242a1a4606
Innocent Asiimwe <asiimwe@outlook.com>
2017-07-05 12:38:55 +0300

    Implement the head_line function of the news app

--- news.py
+++ news.py
@@ -1,6 +1,19 @@
+import requests
+
 def head_lines(source):
     """A function that prints out the latest headline from a select websites"""
-    pass
+    data = {'source': source, 'apikey':'47c95d8398ab44a4b364a7e9463dc4a3'}
+    news = requests.get('https://newsapi.org/v1/articles', params = data)
+
+    news_json = news.json()
+
+    articles = news_json['articles']
+
+    for article in articles:
+        print(article['title'])
+        print(article['description'])
+        print(article['url'])
+        print('')
 
 
 def main():
