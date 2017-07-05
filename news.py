47d0f9ded4fdcfb9279c5e437fed71cd4a32bcba
Innocent Asiimwe <asiimwe@outlook.com>
2017-07-05 12:14:00 +0300

    Define methods the news app will use

---
+++ news.py
@@ -0,0 +1,13 @@
+def head_lines(source):
+    """A function that prints out the latest headline from a select websites"""
+    pass
+
+
+def main():
+    """A function that interacts with the users of the news app, it is an entry point for the applicationn"""
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
